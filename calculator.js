5994139389068f31bae3f53afd4c9c30e74e5b5e
yashgour <69150067+yashgour@users.noreply.github.com>
2020-09-05 00:39:07 +0530

    initial commit

---
+++ calculator.js
@@ -0,0 +1,28 @@
+console.log("calculator")
+let screen=document.getElementById('screen')
+let buttons=document.querySelectorAll('button')
+let screenValue=""
+
+for(item of buttons){
+    item.addEventListener('click',function(e){
+        buttonText=e.target.innerText
+        // console.log(buttonText)
+        if(buttonText=='X'){
+            buttonText='*'
+            screenValue +=buttonText
+            screen.value=screenValue
+
+        }
+        else if(buttonText=='C'){
+            screenValue=""
+            screen.value=screenValue
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenValue)
+        }
+        else{
+            screenValue += buttonText
+            screen.value=screenValue
+        }
+    })
+}
